3f5f19d72795992d7a3a63e1765f6047ed85160e
bachvq <>
2020-12-12 00:33:20 +0700

    remove pods

--- Example/Pods/Target Support Files/Pods-Comier_Example/Pods-Comier_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Comier_Example : NSObject
-@end
-@implementation PodsDummy_Pods_Comier_Example
-@end
--- Example/Pods/Target Support Files/Pods-Comier_Tests/Pods-Comier_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Comier_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_Comier_Tests
-@end
