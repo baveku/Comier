29298f82e8afcf55b7553f0b538612fd6b9f3377
bachvq <>
2021-01-28 17:24:07 +0700

    update public API

--- Comier/Classes/ViewController/BaseViewController.swift
+++ Comier/Classes/ViewController/BaseViewController.swift
@@ -11,6 +11,48 @@ import RxSwift
 
 public let ASBlank = ASStackLayoutSpec.vertical
 
+open class ASDisplayNodePlus: ASDisplayNode {
+    
+    public override init() {
+        super.init()
+        self.automaticallyManagesSubnodes = true
+        self.backgroundColor = .white
+    }
+    
+    public var transitionBlock: ((ASSizeRange, Bool, Bool, (() -> Void)?) -> Void)? = nil
+    public var animateLayoutTransitionBlock: ((ASContextTransitioning) -> Void)? = nil
+    public var didCompleteLayoutTransitionBlock: ((ASContextTransitioning) -> Void)? = nil
+    
+    open override func transitionLayout(with constrainedSize: ASSizeRange, animated: Bool, shouldMeasureAsync: Bool, measurementCompletion completion: (() -> Void)? = nil) {
+        guard let block = transitionBlock else {
+            return super.transitionLayout(
+                with: constrainedSize,
+                animated: animated,
+                shouldMeasureAsync: shouldMeasureAsync,
+                measurementCompletion: completion)
+        }
+        block(constrainedSize, animated, shouldMeasureAsync, completion)
+    }
+    
+    open override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+        super.layoutSpecThatFits(constrainedSize)
+    }
+    
+    open override func layout() {
+        super.layout()
+    }
+    
+    open override func animateLayoutTransition(_ context: ASContextTransitioning) {
+        guard let block = animateLayoutTransitionBlock else {return super.animateLayoutTransition(context)}
+        block(context)
+    }
+    
+    open override func didCompleteLayoutTransition(_ context: ASContextTransitioning) {
+        guard let block = didCompleteLayoutTransitionBlock else {return super.didCompleteLayoutTransition(context)}
+        block(context)
+    }
+}
+
 open class COViewController<VM: ViewModel>: ASDKViewController<ASDisplayNode>, IViewModelViewController {
     public typealias IViewModelType = VM
     
@@ -29,14 +71,33 @@ open class COViewController<VM: ViewModel>: ASDKViewController<ASDisplayNode>, I
         return true
     }
     
+    open var useCustomTransitionAnimation: Bool {
+        return false
+    }
+    
     public required init(viewModel: VM) {
         self.viewModel = viewModel
-        super.init(node: ASDisplayNode())
-        self.node.automaticallyManagesSubnodes = true
-        self.node.backgroundColor = .white
-        self.node.layoutSpecBlock = {[weak self] (node, size) in
+        let mainNode = ASDisplayNodePlus()
+        super.init(node: mainNode)
+        mainNode.backgroundColor = .white
+        mainNode.layoutSpecBlock = {[weak self] (node, size) in
             guard let self = self else {return ASLayoutSpec()}
             return self.layoutSpecThatFits(size)
+        }
+        
+        if useCustomTransitionAnimation {
+            mainNode.transitionBlock = { [weak self] (size, animated, shouldMeasureAsync, completion) in
+                guard let self = self else {return}
+                self.transitionLayout(with: size, animated: animated, shouldMeasureAsync: shouldMeasureAsync, measurementCompletion: completion)
+            }
+            
+            mainNode.animateLayoutTransitionBlock = { [weak self] context in
+                self?.animateLayoutTransition(context)
+            }
+            
+            mainNode.didCompleteLayoutTransitionBlock = { [weak self] context in
+                self?.didCompleteLayoutTransition(context)
+            }
         }
     }
     
@@ -98,4 +159,8 @@ open class COViewController<VM: ViewModel>: ASDKViewController<ASDisplayNode>, I
             // Fallback on earlier versions
         }
     }
+    
+    open func transitionLayout(with constrainedSize: ASSizeRange, animated: Bool, shouldMeasureAsync: Bool, measurementCompletion completion: (() -> Void)? = nil) {}
+    open func animateLayoutTransition(_ context: ASContextTransitioning) {}
+    open func didCompleteLayoutTransition(_ context: ASContextTransitioning) {}
 }
