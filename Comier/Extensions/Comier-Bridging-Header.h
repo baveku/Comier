16ac0ad9b709299e477a4c0d87288708304fab22
bachvq <baveku@gmail.com>
2021-09-30 00:40:39 +0700

    fix FDFullscreenPopGesture path

--- Comier/Extensions/Comier-Bridging-Header.h
+++ Comier/Extensions/Comier-Bridging-Header.h
@@ -1,5 +1,5 @@
 #ifndef inclub_Bridging_Header_h
 #define inclub_Bridging_Header_h
 
-#import "FDFullscreenPopGesture.h"
+#import "FDFullscreenPopGesture/FDFullscreenPopGesture.h"
 #endif /* inclub_Bridging_Header_h */
