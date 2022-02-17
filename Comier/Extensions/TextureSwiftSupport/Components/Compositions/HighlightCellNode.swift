fb6518a8c798aa3d8494cdc43e8a41587c7b548f
bachvq <baveku@gmail.com>
2022-02-17 12:48:48 +0700

    remove lib

--- Comier/Extensions/TextureSwiftSupport/Components/Compositions/HighlightCellNode.swift
+++ Comier/Extensions/TextureSwiftSupport/Components/Compositions/HighlightCellNode.swift
@@ -1,5 +1,5 @@
 import AsyncDisplayKit
-import Descriptors
+
 
 open class HighlightCellNode<D: ASDisplayNode>: NamedDisplayCellNodeBase {
 
--- Comier/Extensions/TextureSwiftSupport/Components/Compositions/TextureSwiftSupport+Compositions.swift
+++ Comier/Extensions/TextureSwiftSupport/Components/Compositions/TextureSwiftSupport+Compositions.swift
@@ -1,2 +1,2 @@
-@_exported import Descriptors
+@_exported 
 
