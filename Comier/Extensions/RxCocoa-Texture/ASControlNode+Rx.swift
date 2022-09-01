b9eef5544ff5f821be04ebd9664e13682fdb7a43
BachVQ <baveku@gmail.com>
2022-09-02 00:17:44 +0700

    remove unused file

--- Comier/Extensions/RxCocoa-Texture/ASControlNode+Rx.swift
+++ Comier/Extensions/RxCocoa-Texture/ASControlNode+Rx.swift
@@ -63,7 +63,7 @@ extension Reactive where Base: ASControlNode {
 
             return Disposables.create(with: controlTarget.dispose)
         }
-        .takeUntil(deallocated)
+			.take(until: deallocated)
 
         let bindingObserver = ASBinder(base, binding: setter)
 
