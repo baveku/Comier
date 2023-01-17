1f00bf123806b335e682cefc5599b4ab26310904
bachvq <baveku@gmail.com>
2023-01-17 13:01:19 +0700

    fix texture package

--- Package.swift
+++ Package.swift
@@ -11,21 +11,21 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/shimastripe/Texture.git", from: .init(3, 1, 1)),
+        .package(url: "https://github.com/baveku/TextureSPM.git", from: "3.1.1"),
         .package(url: "https://github.com/Swinject/Swinject.git", from: "2.8.0"),
         .package(url: "https://github.com/ReactiveX/RxSwift", from: "6.5.0"),
         .package(url: "https://github.com/tristanhimmelman/ObjectMapper", from: "4.2.0"),
         .package(url: "https://github.com/ninjaprox/NVActivityIndicatorView", from: "5.1.1"),
         .package(url: "https://github.com/ra1028/DifferenceKit", from: "1.3.0"),
         .package(url: "https://github.com/google/promises", from: "2.0.0"),
-        .package(url: "https://github.com/Moya/Moya", from: "15.0.3")
+        .package(url: "https://github.com/Moya/Moya", from: "15.0.3"),
     ],
     targets: [
         .target(name: "Comier",
                 dependencies: ["Swinject", "NVActivityIndicatorView", "DifferenceKit", "ObjectMapper", "FDFullscreenPopGesture",
                     .product(name: "Moya", package: "Moya"),
                     .product(name: "RxMoya", package: "Moya"),
-                    .product(name: "AsyncDisplayKit", package: "Texture"),
+                    .product(name: "AsyncDisplayKit", package: "TextureSPM"),
                     .product(name: "Promises", package: "Promises"),
                     .product(name: "RxCocoa", package: "RxSwift"),
                     .product(name: "RxRelay", package: "RxSwift"),
